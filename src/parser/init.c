0485ede74dbea2ed3f2b8de31ed6a6a68965c3ab
gcesar-n <gcesar-n@student.42.ft>
2025-07-17 15:17:49 -0300

    -

--- src/parser/init.c
+++ src/parser/init.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   init.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dteruya <dteruya@student.42.fr>            +#+  +:+       +#+        */
+/*   By: gcesar-n <gcesar-n@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/16 20:03:47 by dteruya           #+#    #+#             */
-/*   Updated: 2025/07/16 20:51:29 by dteruya          ###   ########.fr       */
+/*   Updated: 2025/07/17 15:09:55 by gcesar-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,22 +17,30 @@ static void	init_south(t_info *info, int *i, char *line)
 	int	j;
 	char *value;
 
-	(void)info;
 	*i = *i + 2;
+	info = ft_calloc(1, sizeof(t_info));
 	while (is_wspace(line[*i]))
 		(*i)++;
 	j = *i;
 	while (!is_wspace(line[*i]) && line[*i] != '\n')
 		(*i)++;
 	value = ft_substr(line, j, *i - j);
-	printf("%s\n", value);
+	printf("DEBUG: value antes malloc: %s\n", value);
+	info->south_texture = malloc(sizeof(char) * ft_strlen(value));
+	if (!info->south_texture)
+		print_and_exit("Error!");
+	info->south_texture = value;
+	printf("DEBUG: value malokado:  %s\n", info->south_texture);
+	free(value);
 }
 
 static void	is_ID(t_info *info, char *line, int *i)
 {
-	
 	if (line[*i] == 'S' && line[*i + 1] == 'O' && line[*i + 2] == ' ')
+	{
+		printf("entrouuuu\n");
 		init_south(info, i, line);
+	}
 	// else if (line[*i] == 'E' && line[*(i+1)] == 'A' && line[*(i+2)] == ' ')
 	// 	init_east();
 	// else if (line[*i] == 'W' && line[*(i+1)] == 'E' && line[*(i+2)] == ' ')
