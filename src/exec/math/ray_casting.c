f319c3ef21913f91cedb8f00cd5fd88d2e1f5883
Gabriel Pezoa <gabrielpezoa26@gmail.com>
2025-08-05 10:56:56 -0300

    improved movement and implemented textures

--- src/exec/math/ray_casting.c
+++ src/exec/math/ray_casting.c
@@ -36,7 +36,33 @@ static void direction(t_player *player, t_ray *ray, int index)
 	ray->ray_dir_y = player->dir_y + player->plane_y * ray->cam_x;
 }
 
-static void dda(t_ray *ray, t_player *player)
+static void	set_dda_step(t_player *player, t_ray *ray)
+{
+	if (ray->ray_dir_x < 0)
+	{
+		ray->step_x = -1;
+		ray->side_dist_x = (player->position_x - ray->map_x) * ray->delta_x;
+	}
+	else
+	{
+		ray->step_x = 1;
+		ray->side_dist_x = (ray->map_x + 1.0 - player->position_x) \
+			* ray->delta_x;
+	}
+	if (ray->ray_dir_y < 0)
+	{
+		ray->step_y = -1;
+		ray->side_dist_y = (player->position_y - ray->map_y) * ray->delta_y;
+	}
+	else
+	{
+		ray->step_y = 1;
+		ray->side_dist_y = (ray->map_y + 1.0 - player->position_y) \
+			* ray->delta_y;
+	}
+}
+
+static void	dda(t_ray *ray, t_player *player)
 {
 	ray->map_x = (int)player->position_x;
 	ray->map_y = (int)player->position_y;
@@ -48,26 +74,7 @@ static void dda(t_ray *ray, t_player *player)
 		ray->delta_y = 1e30;
 	else
 		ray->delta_y = fabs(1 / ray->ray_dir_y);
-	if (ray->ray_dir_x < 0)
-	{
-		ray->step_x = -1;
-		ray->side_dist_x = (player->position_x - ray->map_x) * ray->delta_x;
-	}
-	else
-	{
-		ray->step_x = 1;
-		ray->side_dist_x = (ray->map_x + 1.0 - player->position_x) * ray->delta_x;
-	}
-	if (ray->ray_dir_y < 0)
-	{
-		ray->step_y = -1;
-		ray->side_dist_y = (player->position_y - ray->map_y) * ray->delta_y;
-	}
-	else
-	{
-		ray->step_y = 1;
-		ray->side_dist_y = (ray->map_y + 1.0 - player->position_y) * ray->delta_y;
-	}
+	set_dda_step(player, ray);
 }
 
 void	loop_dda(t_ray *ray, t_map *map)
@@ -114,50 +121,84 @@ void	calculate_wall(t_ray *ray)
 		ray->end = HEIGHT - 1;
 }
 
-void	draw_column(t_map *map, int x)
+static void	draw_ceiling_and_floor(t_map *map, int x)
 {
-	int			y;
-	int			color;
-	double		wall_x;
-	int			tex_x;
-	int			tex_y;
-	double		step;
-	double		tex_pos;
-	t_data_img	*texture;
+	int	y;
+	int	ceiling_color;
+	int	floor_color;
 
+	ceiling_color = rgb_to_int(map->info->ceiling_color[0], \
+		map->info->ceiling_color[1], map->info->ceiling_color[2]);
+	floor_color = rgb_to_int(map->info->floor_color[0], \
+		map->info->floor_color[1], map->info->floor_color[2]);
 	y = 0;
 	while (y < map->ray.start)
 	{
-		my_mlx_pixel_put(map->img, x, y, rgb_to_int(map->info->ceiling_color[0], map->info->ceiling_color[1], map->info->ceiling_color[2]));
-		y++;
-	}
-	if (map->ray.side == 0)
-		wall_x = map->player->position_y + map->ray.wall_dist * map->ray.ray_dir_y;
-	else
-		wall_x = map->player->position_x + map->ray.wall_dist * map->ray.ray_dir_x;
-	wall_x -= floor((wall_x));
-	texture = get_texture(map);
-	tex_x = (int)(wall_x * (double)texture->width);
-	if ((map->ray.side == 0 && map->ray.ray_dir_x > 0) || (map->ray.side == 1 && map->ray.ray_dir_y < 0))
-		tex_x = texture->width - tex_x - 1;
-	step = 1.0 * texture->height / map->ray.line_height;
-	tex_pos = (map->ray.start - HEIGHT / 2 + map->ray.line_height / 2) * step;
-	y = map->ray.start;
-	while (y < map->ray.end)
-	{
-		tex_y = (int)tex_pos & (texture->height - 1);
-		tex_pos += step;
-		color = get_texture_color(texture, tex_x, tex_y);
-		my_mlx_pixel_put(map->img, x, y, color);
+		my_mlx_pixel_put(map->img, x, y, ceiling_color);
 		y++;
 	}
 	y = map->ray.end;
 	while (y < HEIGHT)
 	{
-		my_mlx_pixel_put(map->img, x, y, rgb_to_int(map->info->floor_color[0], map->info->floor_color[1], map->info->floor_color[2]));
+		my_mlx_pixel_put(map->img, x, y, floor_color);
 		y++;
 	}
 }
+
+static int	calculate_texture_x(t_map *map, t_data_img *texture, \
+	double wall_x)
+{
+	int	tex_x;
+
+	tex_x = (int)(wall_x * (double)texture->width);
+	if ((map->ray.side == 0 && map->ray.ray_dir_x < 0) || \
+		(map->ray.side == 1 && map->ray.ray_dir_y > 0))
+		tex_x = texture->width - tex_x - 1;
+	return (tex_x);
+}
+
+static double	get_wall_hit_coordinate(t_map *map)
+{
+	double	wall_x;
+
+	if (map->ray.side == 0)
+		wall_x = map->player->position_y + map->ray.wall_dist * \
+			map->ray.ray_dir_y;
+	else
+		wall_x = map->player->position_x + map->ray.wall_dist * \
+			map->ray.ray_dir_x;
+	wall_x -= floor(wall_x);
+	return (wall_x);
+}
+
+static void	draw_textured_wall(t_map *map, int x)
+{
+	t_data_img	*texture;
+	int			tex_x;
+	int			tex_y;
+	int			y;
+	int			color;
+
+	texture = get_texture(map);
+	tex_x = calculate_texture_x(map, texture, get_wall_hit_coordinate(map));
+	y = map->ray.start;
+	while (y < map->ray.end)
+	{
+		tex_y = (int)(((y - (double)HEIGHT / 2 + \
+			(double)map->ray.line_height / 2) * texture->height) / \
+			(double)map->ray.line_height);
+		color = get_texture_color(texture, tex_x, tex_y);
+		my_mlx_pixel_put(map->img, x, y, color);
+		y++;
+	}
+}
+
+void	draw_column(t_map *map, int x)
+{
+	draw_ceiling_and_floor(map, x);
+	draw_textured_wall(map, x);
+}
+
 void	update_player_position(t_map *map)
 {
 	if (map->player->is_moving_forward)
